3963e4498b183a1ac9355496023911ced4b533de
Alisa Ono <alisao@mit.edu>
2017-06-03 19:25:02 -0700

    bug fix

--- routes/index.js
+++ routes/index.js
@@ -28,9 +28,16 @@ if (process.env.NODE_ENV) { // Running on production server
 
   firebaseAdmin.initializeApp({
 	  credential: firebaseAdmin.credential.cert({
+		  "type": "service_account",
+		  "project_id": "nesterly-website",
 	    "private_key": process.env.FIREBASE_PRIVATE_KEY,
 	    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-	    "client_email": process.env.FIREBASE_CLIENT_EMAIL
+	    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+		  "client_id": process.env.FIREBASE_CLIENT_ID,
+		  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+		  "token_uri": "https://accounts.google.com/o/oauth2/token",
+		  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+		  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-xxz3e%40nesterly-website.iam.gserviceaccount.com"
 	  }),
 	  databaseURL: "https://nesterly-website.firebaseio.com"
 	});
